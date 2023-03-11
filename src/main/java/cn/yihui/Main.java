d0bcb931ccaf6cf7e393c0906c195ec7759050d8
liangYiHui <156614618@qq.com>
2023-03-11 19:06:21 +0800

    init

---
+++ src/main/java/cn/yihui/Main.java
@@ -0,0 +1,11 @@
+package cn.yihui;
+
+/**
+ * ● @author: YiHui
+ * ● @date: Created in ${TIME}  ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
